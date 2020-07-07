c1bb8b85295c123d6b5fee5da15d6846808a253b
Luna <1017929760@qq.com>
2020-07-07 23:10:43 +0800

    deploy

---
+++ precache-manifest.0b7423551dd3c0ad9f10a60ff52736df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfe8584ecdf493a65c3",
+    "url": "/dy-website/css/app.e5b64b52.css"
+  },
+  {
+    "revision": "a2a5e182c5239d9554eed953e0050edd",
+    "url": "/dy-website/index.html"
+  },
+  {
+    "revision": "7bfe8584ecdf493a65c3",
+    "url": "/dy-website/js/app.b7506507.js"
+  },
+  {
+    "revision": "540fa6c2b0a714a5034b",
+    "url": "/dy-website/js/chunk-vendors.4635c767.js"
+  },
+  {
+    "revision": "3fac5e9ed2a34d2ba97bf1abb94c0b43",
+    "url": "/dy-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dy-website/robots.txt"
+  }
+]);
